292c4d54752743ce324949ab9f0d6d387c4d8979
Paolo Elia <92871203+ImPaoloEliaX0@users.noreply.github.com>
2022-02-09 18:17:19 +0100

    Esercizio svolto

---
+++ main.js
@@ -0,0 +1,8 @@
+const app = new Vue({
+    el: "#root",
+    data: {
+      title: "hello vue!",
+      message: "This is my first page with Vue",
+      image: "https://v2.vuejs.org/images/logo.svg"
+    }
+  })
